11298b7cad9a15158a56f9e2bc52f1be911366ea
afbrilian <af.brilian@gmail.com>
2025-11-03 15:54:54 +0700

    chore: prod sentry env

--- apps/api/config/prod.exs
+++ apps/api/config/prod.exs
@@ -9,3 +9,8 @@ config :logger, level: :info
 # Do not include debugger and annotate Ecto queries
 config :phoenix, :stacktrace_depth, 20
 config :phoenix, :plug_init_mode, :runtime
+
+# Sentry configuration - environment_name is required for Sentry to start
+# This will be overridden in runtime.exs based on SENTRY_DSN availability
+config :sentry,
+  environment_name: "production"
