28c0ae4023c750b59f566b858bb721ff08331b94
afbrilian <af.brilian@gmail.com>
2025-11-03 15:12:04 +0700

    ci: add c run time and bind to ipv4

--- apps/api/config/runtime.exs
+++ apps/api/config/runtime.exs
@@ -39,11 +39,8 @@ if config_env() == :prod do
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :emailgator_api, EmailgatorWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
-    http: [
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: port
-    ],
+    url: [host: host, scheme: "https", port: 443],
+    http: [ip: {0, 0, 0, 0}, port: port],
     secret_key_base: secret_key_base,
     server: true
 
