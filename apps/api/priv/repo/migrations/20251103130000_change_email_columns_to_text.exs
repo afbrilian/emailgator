fd712d8f6bb27ff00c3067d9fb82c051e7957725
afbrilian <af.brilian@gmail.com>
2025-11-03 20:04:14 +0700

    chore: new email table migration

---
+++ apps/api/priv/repo/migrations/20251103130000_change_email_columns_to_text.exs
@@ -0,0 +1,21 @@
+defmodule Emailgator.Repo.Migrations.ChangeEmailColumnsToText do
+  use Ecto.Migration
+
+  def up do
+    alter table(:emails) do
+      modify :subject, :text
+      modify :from, :text
+      modify :gmail_message_id, :text
+      modify :unsubscribe_urls, {:array, :text}, from: {:array, :string}
+    end
+  end
+
+  def down do
+    alter table(:emails) do
+      modify :subject, :string
+      modify :from, :string
+      modify :gmail_message_id, :string
+      modify :unsubscribe_urls, {:array, :string}, from: {:array, :text}
+    end
+  end
+end
